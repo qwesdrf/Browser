0d3bb3724d5ebbac9d9597d343355b7e985897e0
egolodnikov <w8mepls23@gmail.com>
2016-02-16 17:51:28 +0700

    DDDDDDDDDD

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -1,10 +1,9 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace WpfApplication2
+namespace WpfApplication1
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
@@ -33,13 +32,34 @@ namespace WpfApplication2
                 WebBrowser.Navigate("http://" + Search + TextBox1.Text);
             }
         }
+        
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string correct = TextBox.Text.Replace("www.", "");
+                correct = TextBox.Text.Replace("http://", "");
+                correct = correct.Insert(0, "http://");
+                WebBrowser.Navigate(correct);
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Searchs.Add(TextBox2.Text);
+                ComboBox.ItemsSource = Searchs;
+                TextBox2.Text = "";
+            }
+            
+        }
 
         private void button1_Click_1(object sender, RoutedEventArgs e)
         {
             Searchs.Add(TextBox2.Text);
             ComboBox.ItemsSource = Searchs;
             TextBox2.Text = "";
-
         }
 
         private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -51,6 +71,11 @@ namespace WpfApplication2
         {
             WpfApplication1.Properties.Settings.Default.Directory = TextBox2.Text;
             WpfApplication1.Properties.Settings.Default.Save();
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            WebBrowser.Navigate("http://"+ComboBox.Text+TextBox1.Text);
         }
     }
 }

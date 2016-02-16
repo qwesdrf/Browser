3dccb4e634f92f3d52b805d3f46e9c2681a429f9
egolodnikov <w8mepls23@gmail.com>
2016-02-16 17:48:18 +0700

    Revert ".pp"
    This reverts commit f9614f4f83cd9e0c39fd72cfc8cc0e7d723ef826.

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace WpfApplication1
+namespace WpfApplication2
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
@@ -32,34 +33,13 @@ namespace WpfApplication1
                 WebBrowser.Navigate("http://" + Search + TextBox1.Text);
             }
         }
-        
-        private void textBox_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                string correct = TextBox.Text.Replace("www.", "");
-                correct = TextBox.Text.Replace("http://", "");
-                correct = correct.Insert(0, "http://");
-                WebBrowser.Navigate(correct);
-            }
-        }
-
-        private void textBox2_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                Searchs.Add(TextBox2.Text);
-                ComboBox.ItemsSource = Searchs;
-                TextBox2.Text = "";
-            }
-            
-        }
 
         private void button1_Click_1(object sender, RoutedEventArgs e)
         {
             Searchs.Add(TextBox2.Text);
             ComboBox.ItemsSource = Searchs;
             TextBox2.Text = "";
+
         }
 
         private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -71,11 +51,6 @@ namespace WpfApplication1
         {
             WpfApplication1.Properties.Settings.Default.Directory = TextBox2.Text;
             WpfApplication1.Properties.Settings.Default.Save();
-        }
-
-        private void button_Click_1(object sender, RoutedEventArgs e)
-        {
-            WebBrowser.Navigate("http://"+ComboBox.Text+TextBox1.Text);
         }
     }
 }
